22db582b96f5829b055b6e5d1088e9aab2503e0b
Leonardo Filgueira <33987660+leo-filgueira@users.noreply.github.com>
2018-05-21 14:18:25 -0300

    correcao leaflet output
    Corrigindo tabName (razao pela qual leaflet nao era plotado) e ajustando altura do mapa

--- dashboard/SER/ui.R
+++ dashboard/SER/ui.R
@@ -7,16 +7,14 @@ ui <- dashboardPage(
   dashboardHeader(title = "Exemplo SER"),
   dashboardSidebar(
     sidebarMenu(
-      menuItem("mapas", tabName = "Mapas", icon = icon("map")),
+      menuItem("Mapa", tabName = "mapas", icon = icon("map")),
       menuItem("dygraphs", tabName = "dygraphs", icon = icon("chart-line"))
     )
   ),
   dashboardBody(
     tabItems(
       tabItem(tabName = "mapas",
-              fluidRow(
-                leafletOutput("mapa_leaflet", width = "75%", height = "200px")
-              )  
+                leafletOutput("mapa_leaflet", height = 600)
       ),
       tabItem(tabName = "dygraphs",
            h1("Dygraph")        
